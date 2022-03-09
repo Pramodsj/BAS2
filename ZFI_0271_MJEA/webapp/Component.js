e072fc84cff99d6c28660cd9774d9acbe3177425
PRAMOD S J <pramod.sj86@gmail.com>
2022-03-09 07:54:47 +0000

    4th commit

---
+++ ZFI_0271_MJEA/webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("ZFI_0271_MJEA.zfi_0271_mjea.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});

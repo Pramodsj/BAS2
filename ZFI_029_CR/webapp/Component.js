f978871321e547ae0de5c6057a508420147dd1ec
PRAMOD S J <pramod.sj86@gmail.com>
2022-03-09 07:40:47 +0000

    first commit

---
+++ ZFI_029_CR/webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("ZFI_029_CF.zfi_029_cr.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
---
+++ ZFI_029_CR/webapp/ext/controller/ListReportExt.controller.js
@@ -0,0 +1,5 @@
+sap.ui.controller("ZFI_029_CF.zfi_029_cr.ext.controller.ListReportExt", {
+
+	onClickActionzfi_cov_cash1: function (oEvent) {},
+	onClickActionzfi_cov_cash2: function (oEvent) {}
+});
